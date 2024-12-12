ddd9742b000b1516e04189584bd9b65c2f953be1
danieleBattistelli <bomber81@gmail.com>
2024-12-12 15:14:26 +0100

    feat: fix inport

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,19 @@
-
+import tasks from "./data/data";
 
 function App() {
-  const tasks = [
-    {
-      id: 1,
-      title: "Implementare la homepage",
-      priority: 1,
-      estimatedTime: 120,
-      state: "completed"
-    },
-    {
-      id: 2,
-      title: "Sviluppare il menu di navigazione",
-      priority: 2,
-      estimatedTime: 60,
-      state: "completed"
-    },
-    {
-      id: 3,
-      title: "Creare il footer del sito",
-      priority: 3,
-      estimatedTime: 30,
-      state: "completed"
-    },
-    {
-      id: 4,
-      title: "Ottimizzare le performance",
-      priority: 1,
-      estimatedTime: 180,
-      state: "completed"
-    },
-    {
-      id: 5,
-      title: "Scrivere test per i componenti",
-      priority: 2,
-      estimatedTime: 90,
-      state: "completed"
-    },
-    {
-      id: 6,
-      title: "Implementare la pagina dei contatti",
-      priority: 3,
-      estimatedTime: 60,
-      state: "completed"
-    },
-    {
-      id: 7,
-      title: "Aggiungere animazioni CSS",
-      priority: 2,
-      estimatedTime: 30,
-      state: "backlog"
-    },
-    {
-      id: 8,
-      title: "Integrare l'API di autenticazione",
-      priority: 1,
-      estimatedTime: 120,
-      state: "in_progress"
-    },
-    {
-      id: 9,
-      title: "Aggiornare la documentazione",
-      priority: 3,
-      estimatedTime: 60,
-      state: "backlog"
-    },
-    {
-      id: 10,
-      title: "Rifattorizzare il codice CSS",
-      priority: 2,
-      estimatedTime: 90,
-      state: "in_progress"
-    }
-  ];
+
   const TaskManager = () => {
     const completedTask = tasks.filter(task => task.state === 'completed');
     const currentTask = tasks.filter(task => task.state !== 'completed');
     return (
       <div>
-        <h1 style={{ color: 'black', backgroundColor: 'yellow' }}>Task Manager </h1>
+        <h1 >Task Manager </h1>
         <div>
           <h2>Current Tasks ({currentTask.length})</h2>
           <ul>
             {currentTask.map(task =>
               <li key={task.id}>
-                <strong>{task.title}</strong> <strong style={{ color: 'red' }}>{task.state}</strong><br />
+                <strong>{task.title}</strong> <span className={`state ${task.state}`}>{task.state}</span><br />
                 Priority: {task.priority} <br />
                 Est. Time {task.estimatedTime} <br />
               </li>)
@@ -96,7 +25,7 @@ function App() {
           <ul>
             {completedTask.map(task =>
               <li key={task.id}>
-                <strong>{task.title}</strong> <strong style={{ color: 'green' }}>{task.state}</strong><br />
+                <strong>{task.title}</strong> <strong className={`state ${task.state}`}>{task.state}</strong><br />
                 Priority: {task.priority} <br />
                 Est. Time {task.estimatedTime} <br />
               </li>)
